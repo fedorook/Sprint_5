2302ead4ab5f87961f7500171570377aa4ba43ef
Fedorook <fedorook@gmail.com>
2025-03-31 21:17:45 +0800

    Fixed the URLs styling"

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 class Config:
-    URL = 'https://stellarburgers.nomoreparties.site/'
-    LOGIN_URL = 'https://stellarburgers.nomoreparties.site/login'
-    REGISTRATION_URL = 'https://stellarburgers.nomoreparties.site/register'
+    URL = 'https://stellarburgers.nomoreparties.site'
+    LOGIN_URL = f'{URL}/login'
+    REGISTRATION_URL = f'{URL}/register'
